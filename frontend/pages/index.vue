7cb1d2c0b01a8661860d714f19f853dddc27e57d
Drew Hite <drew@drewhite.org>
2025-01-10 13:31:39 -0500

    navigation seems to be working better

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from 'vue';
-import { useUserStore } from "~/stores/UserStore";
+import { useUserStore } from "~/stores/UserStore.js";
 const userStore = useUserStore();
 const router = useRouter();
 
