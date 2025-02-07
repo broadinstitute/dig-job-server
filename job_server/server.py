8cd2f953cc9a5a3157e24fdd5efa20440b1b8dc9
Drew Hite <drew@drewhite.org>
2025-02-07 12:32:29 -0500

    fixing cors

--- job_server/server.py
+++ job_server/server.py
@@ -38,7 +38,7 @@ def cli_serve(port):
     app = create_app()
     origins = [
         "http://localhost:3000",
-        "http://local.kpndataregistry.org:3000",
+        "https://local.kpndataregistry.org:8000",
         "https://ldserver.kpndataregistry.org"
     ]
     app.add_middleware(
