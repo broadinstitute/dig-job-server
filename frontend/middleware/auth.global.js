b0a2497b5cf48dec24516a3c4feb93083ba38b52
Quy <quy@broad.dev>
2025-03-26 12:52:28 -0400

    no callWithNuxt needed anymore
    issue #14269 is fixed

--- frontend/middleware/auth.global.js
+++ frontend/middleware/auth.global.js
@@ -1,26 +1,23 @@
 import { useUserStore } from "~/stores/UserStore";
-import { callWithNuxt } from "#app";
-
 // so annoying https://github.com/nuxt/nuxt/issues/14269
+// Fixed now! https://github.com/unjs/unctx/pull/28
 export default defineNuxtRouteMiddleware(async (to) => {
     const config = useRuntimeConfig();
     if (config.public.skipAuth) {
         return;
     }
-    const nuxtApp = useNuxtApp();
-    if(to.path.startsWith('/login')){
+
+    if (to.path.startsWith("/login")) {
         return;
     }
     const userStore = useUserStore();
     const isLoggedIn = await userStore.isUserLoggedIn();
-
 
     if (userStore.user) {
         return;
     }
 
     if (!isLoggedIn) {
-
-        return callWithNuxt(nuxtApp, navigateTo, ['/login?redirect=' + to.path]);
+        return navigateTo("/login?redirect=" + to.path);
     }
 });
